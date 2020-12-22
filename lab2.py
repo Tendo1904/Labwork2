bdf8b28cdb8bdf7e69acd3902922b70e5663eaab
Tendo1904 <71323590+Tendo1904@users.noreply.github.com>
2020-12-22 15:59:34 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,41 +1,38 @@
 import csv
-print('What genre do you prefer?')
-_genre = input()
-print('Which platform you use to play?')
-_platform = input()
-print('Single- or Multiplayer')
-_type = input()
+genre = input('What genre do you prefer?\n')
+platform = input('Which platform you use to play?\n')
+type_ = input('Single- or Multiplayer?\n')
 status = True
 
-if _genre == '' and _platform == '' and _type == '':
+if not genre and not platform and not type_:
 	print('Sorry I don`t what I`m looking for...')
 	status = False
-if _genre == '' and _platform != '' and _type != '':
-	_genre = _platform
-if _genre != '' and _platform == '' and _type != '':
-	_platform = _genre
-if _genre != '' and _platform != '' and _type == '':
-	_type = _platform
-if _genre == '' and _platform == '' and _type != '':
-	_genre = _type
-	_platform = _type
-if _genre != '' and _platform == '' and _type == '':
-	_platform = _genre
-	_type = _genre
-if _genre == '' and _platform != '' and _type == '':
-	_genre = _platform 
-	_type = _platform
+if not genre and platform and type_:
+	genre = platform
+if genre and not platform and type_:
+	platform = genre
+if genre and platform and not type_:
+	type_ = platform
+if not genre and not platform and type_:
+	genre = type_
+	platform = type_
+if genre and not platform and not type_:
+	platform = genre
+	type_ = genre
+if not genre and platform and not type_:
+	genre = platform
+	type_ = platform
 
-def game_suggester(_genre,_platform,_type):
+def game_suggester(genre,platform,type_):
 	if status == True:
 		with open ('steam.csv', newline = '') as f:
 			a = []
 			spam = csv.reader(f)
 			for row in spam:
-				if (_genre) and (_platform) and (_type) in row:
+				if (genre) and (platform) and (type_) in row:
 					a.append(row[1])
 			a = list(dict.fromkeys(a))
 		with open ('final.txt', 'w') as fin:
 			for i in range (1,len(a)):
 				fin.write(str(a[i]) + '\n')
-game_suggester(_genre,_platform,_type)	
+game_suggester(genre, platform, type_)
